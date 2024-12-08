27f0abfa3135bf1a994c87e8c718a7f57587fcd3
PhamDy <163091011+PhamDyHp@users.noreply.github.com>
2024-12-08 18:06:59 +0700

    base BE

---
+++ Backend/NotificationService/src/test/java/com/projectsem4/NotificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsem4.NotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

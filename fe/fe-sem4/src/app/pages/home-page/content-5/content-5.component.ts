112d9bffe161dc1cd6ac89c8c34b6b26498d15a2
tannt <129242298+k3sojuuu@users.noreply.github.com>
2025-01-07 10:32:34 +0700

    tannt-1/7

---
+++ fe/fe-sem4/src/app/pages/home-page/content-5/content-5.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import * as AOS from 'aos';
+
+@Component({
+  selector: 'app-content-5',
+  templateUrl: './content-5.component.html',
+  styleUrl: './content-5.component.css'
+})
+export class Content5Component {
+ ngOnInit(): void {
+    // setInterval(() => {
+    //   this.nextSlide();
+    // }, 5000);
+
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true
+    })
+  }
+}
